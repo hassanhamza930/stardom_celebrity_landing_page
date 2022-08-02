a828ce3175fa1e32235f06834cf1990e331b5b23
Hamza Hassan <hassanhamza930@gmail.com>
2022-08-02 01:42:13 -0700

    Basic Setup

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Home(){
+    return(
+        <div className="bg-yellow-500 h-screen w-full flex justify-center items-center text-white font-bold">
+                This would be home page.
+        </div>
+    )
+}   
